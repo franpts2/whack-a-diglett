25cfc5ada9228618061c13817025665ab735e6eb
Francisca Portugal <up202303640@up.pt>
2025-02-28 12:08:25 +0000

    Implemented the util_get_MSB function

--- lab2/utils.c
+++ lab2/utils.c
@@ -10,10 +10,12 @@ int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
 }
 
 int(util_get_MSB)(uint16_t val, uint8_t *msb) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  if (msb==NULL) {
+    return 1;
+  }
 
-  return 1;
+  *msb = val >> 8;
+  return 0;
 }
 
 int (util_sys_inb)(int port, uint8_t *value) {
