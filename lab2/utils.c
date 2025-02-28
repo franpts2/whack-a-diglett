b8842abba5636adcd62838c0340233fb187833fd
Francisca Portugal <up202303640@up.pt>
2025-02-28 11:28:06 +0000

    Started util_sys_inb

---
+++ lab2/utils.c
@@ -0,0 +1,27 @@
+#include <lcom/lcf.h>
+
+#include <stdint.h>
+
+int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
+  /* To be implemented by the students */
+  printf("%s is not yet implemented!\n", __func__);
+
+  return 1;
+}
+
+int(util_get_MSB)(uint16_t val, uint8_t *msb) {
+  /* To be implemented by the students */
+  printf("%s is not yet implemented!\n", __func__);
+
+  return 1;
+}
+
+int (util_sys_inb)(int port, uint8_t *value) {
+  uint32_t n;
+  sys_inb(port,&n);
+  *value = n &0xff;
+  /* To be implemented by the students */
+  printf("%s is not yet implemented!\n", __func__);
+
+  return 1;
+}
