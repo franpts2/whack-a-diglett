71976f7bc43bf8954c68fbc9dc699c823cfd27e7
Sara García <up202306877@up.pt>
2025-03-07 11:23:38 +0000

    Added timer_get_conf and timer_display_conf

--- lab2/timer.c
+++ lab2/timer.c
@@ -32,16 +32,43 @@ void (timer_int_handler)() {
 }
 
 int (timer_get_conf)(uint8_t timer, uint8_t *st) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  if (st == NULL) return 1; 
+  uint32_t cmd = TIMER_RB_CMD | TIMER_RB_COUNT_ | TIMER_RB_SEL(timer);
+  if (sys_outb(TIMER_CTRL, cmd) != OK) return 1;
+  int read_port;
+    switch (timer) {
+        case 0:
+            read_port = TIMER_0;
+            break;
+        case 1:
+            read_port = TIMER_1;
+            break;
+        case 2:
+            read_port = TIMER_2;
+            break;
+        default:
+            return 1;
+    }
+    if (util_sys_inb(read_port, st) != 0) return 1;
+  return 0;
 }
 
-int (timer_display_conf)(uint8_t timer, uint8_t st,
-                        enum timer_status_field field) {
+int (timer_display_conf)(uint8_t timer, uint8_t st, enum timer_status_field field) {
   /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
+  union timer_status_field_val val;
+  switch (field) {
+    case tsf_all:
+      val.byte = st;
+      break;
+    case tsf_base:
+      val.bcd = st & BIT(0);
+      break;
+    case tsf_mode:
+    val.count_mode = st & (BIT(1) | BIT(2) | BIT(3));
+    break;
+    case tsf_initial:
+    val.in_mode = st & (BIT(4) | BIT(5));
+  }
+  timer_print_config(timer, field, val);
   return 1;
 }
