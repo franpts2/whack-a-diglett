58096de0048c6d475c0ca2c76dae71e13f45921d
Maria luiza <up202304306@up.pt>
2025-05-20 10:28:10 +0100

    added outline for video_test_pattern function

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -96,11 +96,18 @@ int(video_test_rectangle)(uint16_t mode, uint16_t x, uint16_t y,
 }
 
 int(video_test_pattern)(uint16_t mode, uint8_t no_rectangles, uint32_t first, uint8_t step) {
-  /* To be completed */
-  printf("%s(0x%03x, %u, 0x%08x, %d): under construction\n", __func__,
-         mode, no_rectangles, first, step);
 
-  return 1;
+  if (map_frame_buffer(mode) != 0) return 1;
+
+  if (set_video_mode(mode) != 0) return 1;
+
+  /*functions to implement here*/
+
+  if (esc_key() != 0) return 1;
+  
+  if (vg_exit() != 0) return 1;
+
+  return 0;
 }
 
 int(video_test_xpm)(xpm_map_t xpm, uint16_t x, uint16_t y) {
