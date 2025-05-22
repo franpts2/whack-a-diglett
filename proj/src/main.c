6eb88e1eccfa4830dc306bb68e108c5588f0b5db
Sara García <up202306877@edu.fe.up.pt>
2025-05-22 15:41:20 +0100

    Fixed main and now project runs

--- proj/src/main.c
+++ proj/src/main.c
@@ -5,6 +5,14 @@ int(proj_main_loop)(int argc, char* argv[]) {
   return 0;
 }
 
-int main(int argc, char* argv[]) {
-  return proj_main_loop(argc, argv);
+int main(int argc, char *argv[]) {
+  lcf_set_language("EN-US");
+  lcf_trace_calls("/home/lcom/labs/proj/trace.txt");
+  lcf_log_output("/home/lcom/labs/proj/output.txt");
+
+  if (lcf_start(argc, argv))
+    return 1;
+
+  lcf_cleanup();
+  return 0;
 }
