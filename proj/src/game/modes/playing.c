37ee91633094cfd22a5635a9f9db0b1d317a22e3
Maria luiza <up202304306@up.pt>
2025-05-27 11:49:23 +0100

    added 9 rectangles to be the digletts

--- proj/src/game/modes/playing.c
+++ proj/src/game/modes/playing.c
@@ -8,13 +8,31 @@
 // initialize the playing screen
 void playing_init(void) {
   // pinta o background
-  vg_draw_rectangle(0, 0, 800, 600, 0x04);
+  vg_draw_rectangle(0, 0, 800, 600, 0x325918);
 
-  // titulo ?
-  // draw_text_scaled("WHACK'A DIGGLET", 200, 50, 0xFF, 3);
+  // titulo
+  draw_text_scaled("WHACK'A DIGGLET", 290, 50, 0xFFFFFF, 3);
 
-  // um elemento de exemplo
-  vg_draw_rectangle(450, 300, 100, 100, 0xFF);
+  int rect_width = 60;
+  int rect_height = 80;
+  int spacing = 60; // space between digletts
+
+  int grid_width = 3 * rect_width + 2 * spacing;
+  // int grid_height = 3 * rect_height + 2 * spacing;
+
+  // calculate starting position to center the grid
+  int start_x = (800 - grid_width) / 2;
+  int start_y = 150;
+
+  // 3x3 grid
+  for (int row = 0; row < 3; row++) {
+    for (int col = 0; col < 3; col++) {
+      int x = start_x + col * (rect_width + spacing);
+      int y = start_y + row * (rect_height + spacing);
+
+      vg_draw_rectangle(x, y, rect_width, rect_height, 0x885500); // cor ainda por definir
+    }
+  }
 }
 
 // handles keyboard input in playing mode
