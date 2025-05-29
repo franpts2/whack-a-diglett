9899e86adea05b503b3d747d21464b5676b0390d
Francisca Portugal <up202303640@up.pt>
2025-05-29 16:57:36 +0100

    Fixed from double to triple buffering. Now the dynamic elements are not appearing

--- proj/src/game/modes/playing-modes/playing_mouse.c
+++ proj/src/game/modes/playing-modes/playing_mouse.c
@@ -18,13 +18,14 @@ void draw_mouse_diglett(int index) {
   unsigned int color = dig->visible ? DIGLETT_COLOR : BACKGROUND_COLOR;
   unsigned int bytes_per_pixel = (m_info.BitsPerPixel + 7) / 8;
   
-  // pre-calculate offsets for faster drawing
+  // Pre-calculate offsets for faster drawing
   unsigned int row_stride = m_info.XResolution * bytes_per_pixel;
   unsigned int start_offset = (dig->y * m_info.XResolution + dig->x) * bytes_per_pixel;
 
+  // Draw to current buffer 
+  void *target_buffer = get_current_buffer();
   for (int y = 0; y < dig->height; y++) {
-    unsigned char* row = (unsigned char*)back_buffer + start_offset + y * row_stride;
- 
+    unsigned char* row = (unsigned char*)target_buffer + start_offset + y * row_stride;
     for (int x = 0; x < dig->width; x++) {
       *((uint32_t*)(row + x * bytes_per_pixel)) = color;
     }
@@ -58,19 +59,21 @@ void playing_mouse_update(void) {
   }
 
   if (state_changed || needs_full_redraw) {
+    // First draw to back buffer
     set_drawing_to_back();
     
-    if (needs_full_redraw) {
-      copy_static_to_back();
-      needs_full_redraw = false;
-    }
+    // Get fresh background
+    copy_static_to_back();
     
-    // Batch draw all digletts
+    // Draw all dynamic elements
     for (int i = 0; i < NUM_DIGLETTS; i++) {
       draw_mouse_diglett(i);
     }
-    
     draw_points_counter();
+
+    swap_buffers();
+    
+    needs_full_redraw = false;
   }
 }
 
